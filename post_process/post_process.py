1c925c4e2a5d9ae8599d73ad6d52c164cf14c318
Carmine Silano <carmine@cheersounds.com>
2025-03-01 22:18:43 -0500

    pedalboard added

--- post_process/post_process.py
+++ post_process/post_process.py
@@ -6,7 +6,8 @@ from upsample import process_input
 from stereo_upmix import MonoToStereoUpmixer
 from saturate import dynamic_saturator
 from analysis import analyze_audio
-
+from pedalboard import Pedalboard, Compressor, Distortion, HighShelfFilter, HighpassFilter, PeakFilter, Gain, Chorus, LadderFilter, Phaser, Convolution, Reverb, Delay, Limiter
+from pedalboard.io import AudioFile
 
 # Butterworth High-pass and Low-pass filters
 def butter_filter(data, cutoff, sr, filter_type, order=4):
@@ -70,7 +71,76 @@ def stereo_upmix_stems(npInst, npVox, samplerate, output_dir):
     print("Stereo files saved.")
     return sumInst, sumVox
 
+def process_instrumental(audio, samplerate):
+    fxchain = Pedalboard([
+        Compressor(threshold_db=-1.0, 
+                   ratio=1.5, 
+                   attack_ms=5.0, 
+                   release_ms=100.0),
+        HighpassFilter(cutoff_frequency_hz = 36),
+        PeakFilter(cutoff_frequency_hz = 111, 
+                   q = 0.8, 
+                   gain_db = 2.7),
+        PeakFilter(cutoff_frequency_hz = 406, 
+                   q = 2.0, 
+                   gain_db = -3.5),
+        PeakFilter(cutoff_frequency_hz = 1434, 
+                   q = 2.43, 
+                   gain_db = -1.3),
+        PeakFilter(cutoff_frequency_hz = 3007, 
+                   q = 1.0, 
+                   gain_db = -1.3),
+        PeakFilter(cutoff_frequency_hz = 5220, 
+                   q = 0.63, 
+                   gain_db = -2.5),
+        HighShelfFilter(cutoff_frequency_hz = 11000, 
+                        gain_db = -4.3, 
+                        q = 0.78),
+        #Distortion(drive_db=3),
+        #Gain(gain_db=0)
+        #Limiter(threshold_db=-0.1)
+    ])
 
+    #dist_fxed = distort_exciter(audio, samplerate, drive=16, distortion=33, highpass=4800, wet_mix=-6, dry_mix=0)
+    chain_fxed = fxchain(audio, samplerate)
+    return chain_fxed
+
+def process_vocals(audio, samplerate):
+    fxchain = Pedalboard([
+        HighpassFilter(cutoff_frequency_hz = 100),
+        PeakFilter(cutoff_frequency_hz = 271, 
+                   q = 1.21, 
+                   gain_db = -2.1),
+        PeakFilter(cutoff_frequency_hz = 518, 
+                   q = 0.31, 
+                   gain_db = -0.6),
+        PeakFilter(cutoff_frequency_hz = 949, 
+                   q = 0.84, 
+                   gain_db = -5.0),
+        PeakFilter(cutoff_frequency_hz = 2696, 
+                   q = 1.0, 
+                   gain_db = -2.7),
+        HighShelfFilter(cutoff_frequency_hz = 10334, 
+                        gain_db = 1.1, 
+                        q = 0.21),
+        Delay(delay_seconds=0.06, 
+              feedback=0, 
+              mix=0.1),
+        Reverb(room_size=0.5,
+               damping=0.5,
+               wet_level=0.15,
+               dry_level=1.0,
+               width=1.0,
+               freeze_mode=0.0),
+        #Distortion(drive_db=.2),
+        #Gain(gain_db=0)
+        #Limiter(threshold_db=-0.1)
+    ])
+
+    #dist_fxed = distort_exciter(audio, samplerate)
+    chain_fxed = fxchain(audio, samplerate)
+    #effected = stereo_upmix(chain_fxed, samplerate, 32)
+    return chain_fxed
 
 def post_process_stems(inst_path, 
                        vocal_path, 
@@ -96,13 +166,18 @@ def post_process_stems(inst_path,
 
     satInst = dynamic_saturator(sumInst, 80)
 
+
+    sat_out = (satInst + sumVox) / 2 #saturated inst + vocal
+    save_wav_from_numpy(output_dir + "/sat_sum.wav", sat_out, samplerate)
+
     inst_analysis = analyze_audio(output_dir + "/sum_stereo_inst.wav")
     vox_analysis = analyze_audio(output_dir + "/sum_stereo_vox.wav")
 
-    
+    pbInst = process_instrumental(sumInst, samplerate)
+    pbVox = process_vocals(sumVox, samplerate)
 
-    finalOut = (satInst + sumVox) / 2
-    save_wav_from_numpy(output_dir + "/output.wav", finalOut, samplerate)
+    pubSum = (pbInst + pbVox) / 2
+    save_wav_from_numpy(output_dir + "/pb_sum.wav", pubSum, samplerate)
 
 
 
